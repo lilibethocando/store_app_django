eeb4b3cdfe5e1ce525cf7866d56e965ed997a05d
Lilibeth <lilibethocando@gmail.com>
2024-06-19 19:08:52 -0500

    Custom Admin

--- store/models.py
+++ store/models.py
@@ -13,6 +13,8 @@ class Collection(models.Model):
     def __str__(self) -> str:
         return self.title
 
+    class Meta:
+        ordering = ['title']
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
@@ -23,6 +25,11 @@ class Product(models.Model):
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotions = models.ManyToManyField(Promotion)
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering = ['title']
 
 
 class Customer(models.Model):
